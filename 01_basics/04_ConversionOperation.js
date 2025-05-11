b198f9b92ac04a15920696618d5b1c7b2928aadc
abhii2107 <bhatiaabhishek776@gmail.com>
2025-05-11 06:43:36 +0000

    config done

--- 01_basics/04_ConversionOperation.js
+++ 01_basics/04_ConversionOperation.js
@@ -8,6 +8,41 @@ console.log(number)
 //"33abc" => NaN
 //true=> 1; false => 0
 
-let isLoggedIn = 1;
+let isLoggedIn = "";
 
-let boolean
+let booleanIsLoggedin = Boolean(isLoggedIn)
+console.log(booleanIsLoggedin)
+//1 => true ; 0=>false
+//"" => false
+// "abhi"=>true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+// ************************ Operations *********************************
+
+let value = 3;
+let Negval = -value
+console.log(Negval)
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"3")
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3 = 2+2
+console.log(num2);
+
+let Gamecounter = 100;
+Gamecounter++;
+console.log(Gamecounter)
+
+
+++Gamecounter;
+console.log(Gamecounter)
