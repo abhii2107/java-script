e15361263996fd03252879edab2ac2a22f10212d
abhii2107 <bhatiaabhishek776@gmail.com>
2025-05-15 17:58:56 +0000

    stack and heap

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,33 @@
+// datatypes are further divided into two parts ont he basis of how it is stored and how itcan be accesed
+// primitive and nopn primitive
+// primitive 7: string,number, boolean, undefied, null,symbol , bigint
+// non primitive (reference type)
+// array , objects , function 
+const heros = ["shaktiman","ironman","daredevil"]
+
+// objrct 
+{
+    name  : "abhsihek"
+    age: 212
+
+}
+//function
+const myFunction = function(){
+    console.log(" hello world")
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack(primitive) and heap(non primitive)
+let myName = "abhishek"
+let anotherName = myName
+ anotherName = "sutta"
+console.log(myName)
+console.log(anotherName)
+let userOne = {
+    email: "user@google.com"
+    
+} 
+let usertwo = userOne
+usertwo.email = "abhishek@google.com"
+console.log(userOne.email)
+console.log(usertwo.email)
