afd3e739fbc4d78456ea8fbafdc60de24b76a2bc
abhii2107 <bhatiaabhishek776@gmail.com>
2025-05-19 02:08:37 +0000

    19 may 2025

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const otherNumber = 23.84328
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.7))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(4.9))
+console.log(Math.min(5,3,6,7,6,3,1))
+
+console.log(Math.random())
+console.log( (Math.random()*10) +1)
+console.log( (Math.random()*10) +1)
+
+const min= 10
+const max = 20
