36cb0addfcc012ff28ef0b870ae3e362041fa61b
abhii2107 <bhatiaabhishek776@gmail.com>
2025-05-24 12:28:53 +0000

    24 may 2025

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+
+
+function sayMyName(){
+console.log("a");
+console.log("b");
+console.log("h");
+console.log("i");
+console.log("s");
+console.log("h");
+console.log("e");
+console.log("k");
+}
+//sayMyName() // functio9n execution
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2)
+}
+addTwoNumbers(54,78)
