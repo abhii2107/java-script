3abc0321fec4d22c25f311d0f39e56b3e7743f72
abhii2107 <bhatiaabhishek776@gmail.com>
2025-05-26 17:40:38 +0000

    26 may 2025

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,7 +11,43 @@ console.log("e");
 console.log("k");
 }
 //sayMyName() // functio9n execution
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2)
+// }
 function addTwoNumbers(number1,number2){
-    console.log(number1+number2)
+    // let result = number1+number2
+    // return result
+    return number1+number2
+    
 }
-addTwoNumbers(54,78)
+ const result = addTwoNumbers(5,4)
+ console.log("result:",result)
+ function loginMessage(username){
+    if(username==undefined){ //this can be written as if(!username)
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just Logged in`
+
+ }
+//  console.log(loginMessage("abhishek"))
+//console.log(loginMessage())
+
+// shopping cart when user is adding the items
+function calculateCartPrice(...num1){// by using ... we can wrap usp all the price in an array
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500))
+
+const user ={
+    username : "abhishek",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`usernamew is ${anyObject.username} and price is ${anyObject.price}`)
+
+}
+handleObject(user)
+
+
