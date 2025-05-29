e2e07ccc449dface29a396938fd177b8146b89e0
abhii2107 <bhatiaabhishek776@gmail.com>
2025-05-29 13:01:29 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -2,19 +2,28 @@ const user  = {
     username : "Abhishek",
     price:999,
     welcomeMessage : function(){
-        console.log(`${this.username}, welcome to website`);
-        console.log(this)   
+        //console.log(`${this.username}, welcome to website`);
+       // console.log(this)   
     }
 
 }
 // user.welcomeMessage()
 // user.username = "johan"
 // user.welcomeMessage()
-console.log(this)
+//console.log(this)
 
-function easy (){
-    console.log(this);
-    console.log(this.username) // this can't be used in finction it only works in objects
+// function easy (){
     
+//    console.log(this);
+//     let username = "abhishek"
+//     console.log(this.username) // this can't be used in finction it only works in objects output will be undefined
+    
+// }
+//easy()
+
+//arrow function
+const easy = () => {
+    let username  = "abhishek"
+    console.log(this.username)
 }
 easy()
